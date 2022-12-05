a64dd6022f497df09ee109ba3a5da660b05d2ff2
Craig McCown <craigrmccown@gmail.com>
2022-12-04 21:23:45 -0500

    Implement moving camera

--- src/main.cpp
+++ src/main.cpp
@@ -96,6 +96,26 @@ void processInput(GLFWwindow *window)
     }
 }
 
+glm::mat4 computePvm(float windowWidth, float windowHeight)
+{
+    float t = glfwGetTime();
+
+    // Spin camera around origin Y axis 5 units above XZ plane, 10 units away, at a speed of 1
+    // radian per second
+    float distance = 10.0;
+    glm::mat4 view = glm::lookAt(
+        glm::vec3(sin(t) * distance, 5.0f, cos(t) * distance), // Move camera in a circle above to origin
+        glm::vec3(0.0f, 0.0f, 0.0f), // Point camera towards origin
+        glm::vec3(0.0f, 0.1f, 0.0f) // Tilt camera so that Y axis points up
+    );
+
+    // Use perspective projection with 45 degree field of view
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)windowWidth / windowHeight, 0.1f, 100.0f);
+
+    // Combine view and projection, computed once per frame
+    return projection * view;
+}
+
 int main()
 {
     initializeGLFW();
@@ -135,20 +155,24 @@ int main()
         processInput(window);
 
         // Compute transformation matrix
-        glm::mat4 identity(1.0f);
-        glm::mat4 modelMat = glm::rotate(identity, (float)glfwGetTime() * glm::radians(30.0f), glm::vec3(1, 1, 0));
-        glm::mat4 viewMat = glm::translate(identity, glm::vec3(0, 0, -3));
-        glm::mat4 projectionMat = glm::perspective(glm::radians(45.0f), (float)windowWidth / windowHeight, 0.1f, 100.0f);
+        glm::mat4 pvm = computePvm(windowWidth, windowHeight);
 
         // Fill background color first
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.use();
-        shader.setUniformMat4("model", modelMat);
-        shader.setUniformMat4("view", viewMat);
-        shader.setUniformMat4("projection", projectionMat);
-        cube.draw();
+
+        // Draw 10 cubes in a circle
+        for (int i = 0; i < 10; i ++)
+        {
+            glm::mat4 model(1.0f);
+            model = glm::rotate(model, glm::radians(36.0f) * i, glm::vec3(0.0, 1.0, 0.0));
+            model = glm::translate(model, glm::vec3(3.0f, 0.0f, 0.0f));
+            shader.setUniformMat4("transform", pvm * model);
+
+            cube.draw();
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
--- src/shaders/shader.vert
+++ src/shaders/shader.vert
@@ -5,12 +5,10 @@ layout (location = 1) in vec2 _texCoord;
 
 out vec2 texCoord;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 transform;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(_coord, 1.0);
+    gl_Position = transform * vec4(_coord, 1.0);
     texCoord = _texCoord;
 }
