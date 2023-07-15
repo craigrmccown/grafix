c1c802f420520ecd591bf52caf2d8a06e3402fcb
Craig McCown <craigrmccown@gmail.com>
2023-07-15 14:13:11 -0400

    Track the index of each NFA state

--- libs/slim/src/slim/nfa.cpp
+++ libs/slim/src/slim/nfa.cpp
@@ -26,7 +26,70 @@ namespace slim::nfa
         }
     }
 
-    static Partial buildNfa(const regex::Node &expr, const Alphabet &alphabet)
+    Transition::Transition(int iAlphabet, State *to) : iAlphabet(iAlphabet), to(to) { }
+
+    State::State(int n, int token) : n(n), token(token) { }
+
+    std::shared_ptr<Transition> State::TransitionTo(int iAlphabet, State *to)
+    {
+        std::shared_ptr<Transition> t = std::make_shared<Transition>(iAlphabet, to);
+        transitions.push_back(t);
+        return t;
+    }
+
+    void Partial::Chain(State *s)
+    {
+        // If we try to chain something that has no dangling pointers, we'll end
+        // up with an orphaned state and leak memory. Assert to ensure this
+        // doesn't happen.
+        assert(dangling.size() > 0);
+
+        for (std::shared_ptr<Transition> d : dangling) d->to = s;
+    }
+
+    Nfa::Nfa(const std::vector<std::unique_ptr<regex::Node>> &exprs) : size(0)
+    {
+        // First, construct a global alphabet common across all expressions
+        Alphabet::Buffer buf;
+
+        for (const std::unique_ptr<regex::Node> &expr : exprs)
+        {
+            extractAlphabet(*expr, buf);
+        }
+
+        Alphabet alpha(buf);
+        head = newState();
+
+        // Build an NFA for each expression, then connect each NFA to the entry
+        // state with epsilon transitions.
+        for (int i = 0; i < exprs.size(); i++)
+        {
+            Partial nfa = build(*exprs[i], alpha);
+
+            // Connect dangling pointers to an accepting state whose token is
+            // the index of the expression. We can use the index to identify the
+            // token later.
+            nfa.Chain(newState(i));
+            head->TransitionTo(constants::epsilon, nfa.head);
+        }
+    }
+
+    Nfa::~Nfa()
+    {
+        free(head);
+    }
+
+    State *Nfa::newState()
+    {
+        return newState(-1);
+    }
+
+    State *Nfa::newState(int token)
+    {
+        return new State(size++, token);
+    }
+
+    Partial Nfa::build(const regex::Node &expr, const Alphabet &alphabet)
     {
         switch (expr.kind)
         {
@@ -41,9 +104,9 @@ namespace slim::nfa
                 // S ─┤
                 //    └─ε──> R ──>
 
-                State *s = new State;
-                Partial left = buildNfa(*expr.left, alphabet);
-                Partial right = buildNfa(*expr.right, alphabet);
+                State *s = newState();
+                Partial left = build(*expr.left, alphabet);
+                Partial right = build(*expr.right, alphabet);
 
                 s->TransitionTo(constants::epsilon, left.head);
                 s->TransitionTo(constants::epsilon, right.head);
@@ -62,8 +125,8 @@ namespace slim::nfa
                 //
                 // L ──> R ──>
 
-                Partial left = buildNfa(*expr.left, alphabet);
-                Partial right = buildNfa(*expr.right, alphabet);
+                Partial left = build(*expr.left, alphabet);
+                Partial right = build(*expr.right, alphabet);
                 left.Chain(right.head);
 
                 return Partial{ .head = left.head, .dangling = right.dangling };
@@ -79,8 +142,8 @@ namespace slim::nfa
                 // S ─┤
                 //    └─ε────────>
 
-                State *s = new State;
-                Partial left = buildNfa(*expr.left, alphabet);
+                State *s = newState();
+                Partial left = build(*expr.left, alphabet);
 
                 s->TransitionTo(constants::epsilon, left.head);
 
@@ -100,8 +163,8 @@ namespace slim::nfa
                 // S ─┤<───────ε─┘
                 //    └─ε────────>
 
-                State *s = new State;
-                Partial left = buildNfa(*expr.left, alphabet);
+                State *s = newState();
+                Partial left = build(*expr.left, alphabet);
 
                 left.Chain(s);
                 s->TransitionTo(constants::epsilon, left.head);
@@ -118,8 +181,8 @@ namespace slim::nfa
                 // ┌─ε─────> S ──>
                 // L <─────ε─┘
 
-                State *s = new State;
-                Partial left = buildNfa(*expr.left, alphabet);
+                State *s = newState();
+                Partial left = build(*expr.left, alphabet);
 
                 left.Chain(s);
                 s->TransitionTo(constants::epsilon, left.head);
@@ -132,7 +195,7 @@ namespace slim::nfa
                 // Map left and right to alphabet and add one dangling
                 // transition for each character range
 
-                State *s = new State;
+                State *s = newState();
                 Alphabet::RangeIndex idx = alphabet.Map(expr.left->g, expr.right->g);
 
                 for (int i = idx.i; i < idx.len; i++)
@@ -147,7 +210,7 @@ namespace slim::nfa
                 // Find character range in alphabet representing a single glyph
                 // and add one transition
 
-                State *s = new State;
+                State *s = newState();
                 s->TransitionTo(alphabet.Map(expr.g, expr.g).i, nullptr);
 
                 return Partial{ .head = s, .dangling = s->transitions };
@@ -158,7 +221,7 @@ namespace slim::nfa
                 // well as a transition representing every character not in the
                 // alphabet
 
-                State *s = new State;
+                State *s = newState();
                 s->TransitionTo(constants::negative, nullptr);
 
                 for (int i = 0; i < alphabet.Length(); i++)
@@ -173,7 +236,7 @@ namespace slim::nfa
         }
     }
 
-    static void freeNfa(State *s)
+    void Nfa::free(State *s)
     {
         std::stack<State *> stack;
         std::set<State *> visited;
@@ -198,59 +261,9 @@ namespace slim::nfa
         }
     }
 
-    Transition::Transition(int iAlphabet, State *to) : iAlphabet(iAlphabet), to(to) { }
-
-    State::State(int token) : token(token) { }
-
-    State::State() : State(-1) { }
-
-    std::shared_ptr<Transition> State::TransitionTo(int iAlphabet, State *to)
+    int Nfa::Size() const
     {
-        std::shared_ptr<Transition> t = std::make_shared<Transition>(iAlphabet, to);
-        transitions.push_back(t);
-        return t;
-    }
-
-    void Partial::Chain(State *s)
-    {
-        // If we try to chain something that has no dangling pointers, we'll end
-        // up with an orphaned state and leak memory. Assert to ensure this
-        // doesn't happen.
-        assert(dangling.size() > 0);
-
-        for (std::shared_ptr<Transition> d : dangling) d->to = s;
-    }
-
-    Nfa::Nfa(const std::vector<std::unique_ptr<regex::Node>> &exprs)
-    {
-        // First, construct a global alphabet common across all expressions
-        Alphabet::Buffer buf;
-
-        for (const std::unique_ptr<regex::Node> &expr : exprs)
-        {
-            extractAlphabet(*expr, buf);
-        }
-
-        Alphabet alpha(buf);
-        head = new State;
-
-        // Build an NFA for each expression, then connect each NFA to the entry
-        // state with epsilon transitions.
-        for (int i = 0; i < exprs.size(); i++)
-        {
-            Partial nfa = buildNfa(*exprs[i], alpha);
-
-            // Connect dangling pointers to an accepting state whose token is
-            // the index of the expression. We can use the index to identify the
-            // token later.
-            nfa.Chain(new State(i));
-            head->TransitionTo(constants::epsilon, nfa.head);
-        }
-    }
-
-    Nfa::~Nfa()
-    {
-        freeNfa(head);
+        return size;
     }
 
     const State *Nfa::GetHead() const
